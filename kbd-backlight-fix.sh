91b8741e0c236c5e3fc3c56b59f60f9a1e27458d
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2021-07-11 23:35:22 +0530

    Simplify keyboard backlight fix script

--- kbd-backlight-fix.sh
+++ kbd-backlight-fix.sh
@@ -1,42 +1,15 @@
 #!/bin/bash
 
-# Set keyboard backlight brightness function
-set_brightness() {
-    case $1 in
-        48)
-            asusctl -k off
-            ;;
-        49)
-            asusctl -k low
-            ;;
-        50)
-            asusctl -k med
-            ;;
-        51)
-            asusctl -k high
-            ;;
-    esac
-}
-
 if [ "${1}" == "pre" ]; then
-    # Save current keyboard backlight brightness and turn it off
-    asusctl -k | cut -d " " -f 5 > /tmp/kbd-brightness
-    asusctl -k off
+    # Add delay before sleep, so there is time to fully turn off the backlight
+    sleep 1
 elif [ "${1}" == "post" ]; then
-    # Read and set brightness value set before suspending
-    if [[ -s "/tmp/kbd-brightness" ]]; then
-        brightness=$(cat /tmp/kbd-brightness)
-        set_brightness $brightness
-        rm /tmp/kbd-brightness
+    # Check if keyboard backlight brightness was set to off before suspending
+    brightness=$(cat /sys/class/leds/asus::kbd_backlight/brightness)
+    if [ $brightness -eq 0 ]; then
+        # Fix static brightness issue after wake-up
+        echo 3 > /sys/class/leds/asus::kbd_backlight/brightness  
+        # Turn off keyboard backlight brightness
+        echo 0 > /sys/class/leds/asus::kbd_backlight/brightness  
     fi
-
-    # Delay after wake-up
-    sleep 0.35
-
-    # Get brightness value (set before suspend or during the delay above)
-    brightness=$(asusctl -k | cut -d " " -f 5)
-    asusctl -k high  # Fix static brightness issue after wake-up
-
-    # Set keyboard backlight brightness according to the previously read value 
-    set_brightness $brightness
 fi
